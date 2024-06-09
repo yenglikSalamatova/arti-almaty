a174550743a5c2957efb7373b3394b600f485d13
Enlik Salamatova <yengliksalamatova@gmail.com>
2024-06-09 17:05:55 +0500

    chore: Update .gitignore and supabase configuration

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://pttupsztvmgtnvdshluf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0dHVwc3p0dm1ndG52ZHNobHVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3OTkyNzYsImV4cCI6MjAyNDM3NTI3Nn0.8oQ1SUAxRovnLsKDdYphvdyIl9nNqhe3g2Py7au9_wk";
+export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
